3239925627c3af5e21d719ab241a1063f6d1727d
Saad Malik <saad_malik200@hotmail.com>
2022-10-05 23:27:12 +0200

    ex2 finished

--- index.js
+++ index.js
@@ -28,31 +28,32 @@ const randomColor = function () {
   for (let i = 0; i < 6; i++) {
     let index = Math.floor(Math.random() * hexValues.length);
     hex += hexValues[index];
-    // console.log(random);
   }
-  //   console.log(hex);
+
   return hex;
 };
-// randomColor();
 
 function createItem(color) {
-  //   let li = "";
-  //   li.innerHTML = "";
   for (let i = 0; i < input.value; i++) {
+    const newClr = color();
+    console.log(newClr);
     const li = `<li>
     
-    <div style = 'background:${color()}'>
+    <div style = 'background:${newClr}'>${newClr}
     <p  class = 'cross'>x</p>
     </div>
     </li>`;
     ul.insertAdjacentHTML("beforeend", li);
   }
   boxes.append(ul);
+
   button.addEventListener("click", () => {
     for (let i = 0; i < input.value; i++) {
+      const newClr = color();
+      console.log(newClr);
       const li = `<li>
     
-    <div style = 'background:${color()}'>
+    <div style = 'background:${newClr}'>${newClr}
     <p  class = 'cross'>x</p>
     </div>
     </li>`;
@@ -60,61 +61,21 @@ function createItem(color) {
     }
     boxes.append(ul);
   });
-  //   const li1 = document.createElement("li");
-  //   const li2 = document.createElement("li");
-  //   const li3 = document.createElement("li");
-  //   const div1 = document.createElement("div");
-  //   const div2 = document.createElement("div");
-  //   const div3 = document.createElement("div");
-  //   //   console.log(color);
-  //   const cross1 = document.createElement("p");
-  //   cross1.textContent = "x";
-
-  //   cross1.classList.add("cross");
-  //   const cross2 = document.createElement("p");
-  //   cross2.textContent = "x";
-  //   cross2.classList.add("cross");
-  //   const cross3 = document.createElement("p");
-  //   cross3.textContent = "x";
-  //   cross3.classList.add("cross");
-
-  //   div1.append(cross1);
-  //   div2.append(cross2);
-  //   div3.append(cross3);
-
-  //   div1.style.backgroundColor = color();
-  //   div2.style.backgroundColor = color();
-  //   div3.style.backgroundColor = color();
-
-  //   li1.append(div1);
-  //   li2.append(div2);
-  //   li3.append(div3);
-  //   ul.append(li1, li2, li3);
 }
 
-// createItem(randomColor);
-
 function removeItem() {
-  //   const c = document.querySelector(".cross");
-
   ul.addEventListener("click", (e) => {
     if (e.target.tagName === "P") {
       console.log(e.target.tagName);
       e.target.closest("li").remove();
     }
   });
-  //   cross.forEach((item) => {
-  //     console.log(item);
-  //   });
-  //   console.log(cross);
 }
 
 removeItem();
 
 function init() {
   createItem(randomColor);
-  //   createItem(randomColor);
-  //   createItem(randomColor);
 }
 
 init();
